8db8bd2ef48a7551cd98ed9fcf275ede024b0d5c
Максим <fvvfvfvf.vfvfvfv@yandex.ru>
2020-07-22 20:08:23 +0300

    make basic.html and make login

--- dshop/main/urls.py
+++ dshop/main/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path 
 from .views import home
+from .views import BbLogin
 
 app_name = 'main'
 
 urlpatterns = [
 	path('', home, name = 'home'),
+	path('profile/login/', BbLogin.as_view(), name = 'login'),
 ]
--- dshop/main/views.py
+++ dshop/main/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Rubric, Bb
+from django.contrib.auth.views import LoginView
 
 def home(request):
-	return render(request, 'main/home.html')
+	bbs = Bb.objects.all()[:10]
+	context = {'bbs':bbs}
+	return render(request, 'main/home.html', context)
+
+class BbLogin(LoginView):
+	template_name = 'main/login.html'
 
 # Create your views here.
